6fe6776bd7531a0a0db342ce3727d2f8afdd7146
Hamza ksaier <HamzaKsaier@takiacademyteam.com>
2022-03-21 10:23:29 +0100

    end of styling

--- src/views/Home/Recents_files.jsx
+++ src/views/Home/Recents_files.jsx
@@ -1,71 +1,66 @@
-
 import FILES from '../../constants/files';
 import image from '../../assets/icons/Image.svg';
-import video from '../../assets/icons/Video.svg';
-import File from '../../assets/icons/File.svg';
-import bytesToSize from '../../utils/bytesToSize';
-import starred from '../../assets/icons/Star.svg';
-import {NavLink } from  'react-router-dom';
+import video from '../../assets/icons/Video.svg'; 
+import File from '../../assets/icons/File.svg'; 
+import bytesToSize from'../../utils/bytesToSize'; 
+import starred from '../../assets/icons/Star.svg'; 
+import archived from '../../assets/icons/Box.svg';
+import {NavLink } from 'react-router-dom'; 
 
-
-const RecentFiles = () => {
-    return (
-        <div className="recentFiles">
-            <div className="files">
+const RecentFiles = () => { return (
+<div className="recentFiles">
+    <div className="files">
+        <div className="title">
+            <b>Recent Files</b>
+        </div>
+        <table>
+            <tr>
+                <th></th>
+                <th>Name</th>
+                <th>Date Created</th>
+                <th>Size</th>
+            </tr>
+            {FILES.map((file, ind) => { const month = ["January","February","March","April","May","June","July","August","September","October","November","December"]; let d = new Date(file.createdAt); let type = undefined; if
+            (file.name.startsWith('image')) { type = image; }else if (file.name.startsWith('file')) { type = File; } else { type = video; } return (
+            <tr key={ind}>
+                <td><img src={type} /></td>
+                <td>{`${file.name}.${file.type}`}</td>
+                <td>{`${month[d.getMonth()]} ${d.getDate()}, ${d.getFullYear()}`}</td>
+                <td>{bytesToSize(file.size)}</td>
+            </tr>
+            ) })}
+        </table>
+    </div>
+    <div className="information-files">
+        <div className="starred-archived">
+            <div className="logo-files-starred">
+                <img src={starred} />
+            </div>
+            <div className="title-description">
                 <div className="title">
-                Recent Files 
+                    4 Starred Files
                 </div>
-                <table>
-       <tr>
-         <th></th>
-         <th>Name</th>
-         <th>Date Created</th>
-         <th>Size</th>
-       </tr>
-       {FILES.map((file, ind) => {
-         const month = ["January","February","March","April","May","June","July","August","September","October","November","December"];
-         let d = new Date(file.createdAt);
-         let type = undefined;
-         if (file.name.startsWith('image')) {
-         type = image;
-         }else if (file.name.startsWith('file')) {
-           type = File;
-         } else {
-           type = video;
-         }
-         return (
-             <tr key={ind}>
-             <td><img src = {type}/></td>
-             <td>{`${file.name}.${file.type}`}</td>
-             <td>{`${month[d.getMonth()]} ${d.getDate()}, ${d.getFullYear()}`}</td>
-             <td>{bytesToSize(file.size)}</td>
-           </tr>
-         )
-       })}
-     </table>
-            </div>
-            <div className='information-files'>
-            <div className='starred-files'>
-            <div className='logo-files'>
-                    <img src={starred}/>
-              </div>
-              <div className='title-description'>
-              <div className="title">
-                  4  Starred Files
-             </div>
-             <div className='view-file'> 
-             <NavLink to="/starred" > Go to view</NavLink>
-             </div>
+                <div className="view-file">
+                    <NavLink to="/starred"> Go to view</NavLink>
                 </div>
-             
-          </div>
-             
-            
-              </div>
-            <div className='archived-files'>
-               </div>
             </div>
-      
-    )
-}
+        </div>
+
+        <div className="starred-archived">
+            <div className="logo-files-archived">
+                <img src={archived} />
+            </div>
+            <div className="title-description">
+                <div className="title">
+                   0 Archived Files
+                </div>
+                <div className="view-file">
+                    <NavLink to="/archived"> Go to view</NavLink>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+) } 
 export default RecentFiles;
